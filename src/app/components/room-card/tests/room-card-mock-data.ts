fa7536e307dac005abba2fe58e6a3722941b435e
hiba-malhis <hmalhis@peaxy.com>
2024-02-15 20:07:27 +0200

    add unit  test for RoomCardComponent

---
+++ src/app/components/room-card/tests/room-card-mock-data.ts
@@ -0,0 +1,10 @@
+export const roomMockData = {
+  amenities: [],
+  availability: [],
+  id: 0,
+  roomNumber: 101,
+  type: 'Standard',
+  price: 100,
+  image: 'room-image.jpg',
+  nextAvailableDate: '2024-01-01'
+}
