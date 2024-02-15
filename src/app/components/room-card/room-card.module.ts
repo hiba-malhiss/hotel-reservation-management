42e5ea7450146dceec79cfff899157995a1a2777
hiba-malhis <hmalhis@peaxy.com>
2024-02-15 17:25:16 +0200

    add button to room card

--- src/app/components/room-card/room-card.module.ts
+++ src/app/components/room-card/room-card.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RoomCardComponent } from "./room-card.component";
 import { CardModule } from "primeng/card";
+import { FlexboxModule } from "../../../ui-kit/components/flexbox/src/flexbox.module";
+import { ButtonModule } from "primeng/button";
 
 @NgModule({
   declarations: [RoomCardComponent],
   exports: [RoomCardComponent],
   imports: [
     CommonModule,
-    CardModule
+    CardModule,
+    FlexboxModule,
+    ButtonModule
   ]
 })
 export class RoomCardModule { }
